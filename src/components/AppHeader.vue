3373c4d53f2026a50fb9dba9348357abed29a778
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-12-05 14:51:50 +0100

    Aggiunto componente headernav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,34 @@
 <script>
+import HeaderNav from "./HeaderNav.vue";
 export default {
   name: "AppHeader",
+  components: {
+    HeaderNav,
+  },
 };
 </script>
 
 <template>
-  <h1>header</h1>
+  <div class="container d-flex justify-content-between">
+    <HeaderNav />
+    <ul class="d-flex align-items-center">
+      <li class="mx-3">Home</li>
+      <li class="mx-3">Pages</li>
+      <li class="mx-3">Courses</li>
+      <li class="mx-3">Features</li>
+      <li class="mx-3">Blog</li>
+      <li class="mx-3">Shop</li>
+    </ul>
+    <div>
+      <img src="../assets/dark-logo.png" alt="logo" />
+    </div>
+    <div class="d-flex">
+      <i class="fa-solid fa-cart-shopping"></i>
+      <div>
+        <i class="fa-regular fa-user"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "HeaderNav",
+  data() {
+    return {
+      links: ["HOME", "PAGES", "COURSES", "FEATURES", "BLOG", "SHOP"],
+    };
+  },
+};
+</script>
+
+<template>
+  <nav class="main-nav">
+    <ul class="d-flex justify-content-center">
+      <li v-for="link in links">
+        <a href="#">{{ link }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.main-nav {
+  ul {
+    list-style: none;
+    margin-bottom: 0;
+    padding-left: 0;
+    a {
+      display: inline-block;
+      padding: 20px 10px;
+      text-decoration: none;
+      color: black;
+    }
+  }
+}
+</style>
