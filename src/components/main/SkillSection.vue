5a74e98cff0c75adf36d31b487869723f2d2ddc6
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-12-06 12:41:13 +0100

    Aggiunti elementi in assets

--- src/components/main/SkillSection.vue
+++ src/components/main/SkillSection.vue
@@ -6,9 +6,33 @@ export default {
 
 <template>
   <section class="skills">
-    <div class="container d-flex justify-content-center">
+    <div class="container d-flex justify-content-center position-relative">
+      <img
+        class="position-absolute bottom-0 end-60"
+        src="../../assets/maxcoach-shape-07.png"
+        alt="shape07"
+      />
       <div class="col-left pt-5">
-        <img src="../../assets/stock-full-hd-11-670x450.jpg" alt="stock-full" />
+        <img
+          class="stock"
+          src="../../assets/stock-full-hd-11-670x450.jpg"
+          alt="stock-full"
+        />
+        <img
+          class="shape position-absolute"
+          src="../../assets/maxcoach-shape-05.png"
+          alt="shape05"
+        />
+        <img
+          class="svg position-absolute top-0 start-0"
+          src="../../assets/svg-0.svg"
+          alt="svg0"
+        />
+        <img
+          class="youtube"
+          src="../../assets/icon-youtube-play.png"
+          alt="youtube"
+        />
       </div>
       <div class="col-right pt-5">
         <h6>HOW WE WORK</h6>
@@ -36,7 +60,7 @@ export default {
 .col-right {
   width: 30%;
   margin-left: 120px;
-  margin-top: 30px;
+  margin-top: 50px;
   & h1 {
     font-weight: bold;
     & span {
@@ -53,11 +77,28 @@ export default {
 }
 
 .col-left {
-  width: 50%;
   padding-bottom: 90px;
   position: relative;
-  & img {
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-end;
+  & .stock {
+    width: 90%;
     border-radius: 5px;
+    z-index: 2;
+    padding-right: 80px;
+  }
+  & .youtube {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -40px 0 0 -25px;
+    z-index: 3;
+  }
+  & .shape {
+    top: 80px;
+    left: 0;
+    z-index: 1;
   }
 }
 </style>
