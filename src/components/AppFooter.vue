54a899a1ba0df5307678aba9d48f09c5d4749d82
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-12-07 16:00:21 +0100

    Aggiunto footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,41 @@
 <script>
+import MainFooter from "./MainFooter.vue";
 export default {
   name: "AppFooter",
+  components: {
+    MainFooter,
+  },
 };
 </script>
 
 <template>
-  <h1>footer</h1>
+  <footer>
+    <div class="mycontainer d-flex justify-content-between">
+      <MainFooter />
+    </div>
+    <div>
+      <span class="copyright d-flex justify-content-center"
+        ><i class="fa-regular fa-copyright"></i> 2020 MaxCoach. All Rights
+        Reserved</span
+      >
+      <a href="#top" id="moving-top">
+        <i class="fa-solid fa-arrow-up"></i>
+      </a>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+  background-color: #171621;
+  margin-top: 80px;
+  & .mycontainer {
+    width: 900px;
+    margin: auto;
+  }
+  & .copyright {
+    color: #abab95;
+    font-size: 10px;
+  }
+}
+</style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,135 @@
+<script>
+export default {
+  name: "MainFooter",
+  data() {
+    return {
+      exploreMenu: [
+        {
+          label: "Start here",
+          href: "#",
+        },
+        {
+          label: "Blog",
+          href: "#",
+        },
+        {
+          label: "About us",
+          href: "#",
+        },
+        {
+          label: "Success story",
+          href: "#",
+        },
+        {
+          label: "Courses",
+          href: "#",
+        },
+        {
+          label: "Contact us",
+          href: "#",
+        },
+      ],
+      informationMenu: [
+        {
+          label: "Membership",
+          href: "#",
+        },
+        {
+          label: "Purchase guide",
+          href: "#",
+        },
+        {
+          label: "Privacy policy",
+          href: "#",
+        },
+        {
+          label: "Terms of services",
+          href: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="main-footer container py-5 px-0">
+    <div class="agency">
+      <div class="agency__address">
+        <h5 class="mb-3">Address</h5>
+        <small class="street">382 NE 191st # 87394 Miami, FL 33179-3899</small>
+        <small class="number"
+          >+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</small
+        >
+        <small class="email">support@maxcoach.com</small>
+      </div>
+      <div class="agency__social mt-4">
+        <a href="#">
+          <i class="fa-brands fa-square-facebook"></i>
+        </a>
+        <a href="#">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="#">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+    <nav class="menu-links">
+      <ul class="">
+        <h5 class="mb-3">Explore</h5>
+        <li v-for="link in exploreMenu" class="mb-2">
+          <a :href="link.href">{{ link.label }}</a>
+        </li>
+      </ul>
+    </nav>
+    <nav class="menu-links">
+      <ul class="">
+        <h5 class="mb-3">Information</h5>
+        <li v-for="link in informationMenu" class="mb-2">
+          <a :href="link.href">{{ link.label }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main-footer {
+  display: flex;
+  justify-content: space-between;
+  color: #696969;
+  & h5 {
+    font-size: 15px;
+    font-weight: bolder;
+    color: white;
+  }
+  & .agency__address small {
+    display: block;
+    margin-bottom: 5px;
+  }
+  & .agency__social a {
+    color: #b4b4b4;
+    margin-right: 30px;
+    &:hover {
+      color: white;
+    }
+    & i {
+      font-size: 25px;
+    }
+  }
+  & .menu-links {
+    display: flex;
+    & ul {
+      list-style: none;
+      & li a {
+        color: #696969;
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
